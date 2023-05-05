fde10beb00314eb58720fed91e410f20bf2eb720
root <root@ip-172-31-56-10.us-west-2.compute.internal>
2023-05-05 12:55:21 +0000

    test

--- git.sh
+++ git.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Set variables
+commit_msg=""
+
+# Get commit message from user
+echo "Enter commit message:"
+read commit_msg
+
+
 git add .
 
-git commit -m "$commit msg"
+git commit -m "$commit_msg"
 
 git push origin
